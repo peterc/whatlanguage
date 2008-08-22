6b4684af908432721e072ee9af387132f94060a5
Henrik N <henrik@nyh.se>
2008-08-22 23:21:57 +0200

    Add the Swedish language.
    There is a script in wordlists/generators/swedish.rb to regenerate the wordlist from a CC-licensed source. The build_lang_from_wordlists script ignores the generators directory.

--- build_lang_from_wordlists.rb
+++ build_lang_from_wordlists.rb
@@ -6,6 +6,7 @@ languages_folder = File.join(File.dirname(__FILE__), "lang")
 wordlists_folder = File.join(File.dirname(__FILE__), "wordlists")
 
 Dir.entries(wordlists_folder).grep(/\w/).each do |lang|
+  next if lang == 'generators'
   puts "Doing #{lang}"
   filter = WhatLanguage.filter_from_dictionary(File.join(wordlists_folder, lang))
   File.open(File.join(languages_folder, lang + ".lang"), 'w') { |f| f.write filter.dump }
