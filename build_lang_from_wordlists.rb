0a550fa6ce1aae5abb0dd4f4a541ef661e7160ac
peterc <peter@peterc.org>
2008-09-28 17:31:34 -0700

    Added darragh's suggestion to force binary mode for Windows compatibility

--- build_lang_from_wordlists.rb
+++ build_lang_from_wordlists.rb
@@ -9,5 +9,5 @@ Dir.entries(wordlists_folder).grep(/\w/).each do |lang|
   next if lang == 'generators'
   puts "Doing #{lang}"
   filter = WhatLanguage.filter_from_dictionary(File.join(wordlists_folder, lang))
-  File.open(File.join(languages_folder, lang + ".lang"), 'w') { |f| f.write filter.dump }
+  File.open(File.join(languages_folder, lang + ".lang"), 'wb') { |f| f.write filter.dump }
 end
